812e6fcefd6e8671aeda16cfca666f4bc51b7443
unknown <>
2017-02-05 00:13:08 +0800

    update

--- demo/CascadeMultiSelectDemo.js
+++ demo/CascadeMultiSelectDemo.js
@@ -9,65 +9,90 @@
 const React = require('react');
 const CascadeMultiSelect = require('../src');
 
-const options = [{
-  value: 'zhejiang',
-  label: '浙江',
-  children: [{
-    value: 'hangzhou',
-    label: '杭州',
+const options = [
+  {
+    value: 'zhejiang',
+    label: '浙江',
     children: [{
-      value: 'xihu',
-      label: '西湖'
+      value: 'hangzhou',
+      label: '杭州杭州杭州杭州杭州杭州杭州杭州杭州杭州',
+      children: [{
+        value: 'xihu',
+        label: '西湖'
+      },{
+        value: 'bingjiang',
+        label: '滨江'
+      }],
+    },{
+      value: 'ningbo',
+      label: '宁波',
+      children: [{
+        value: 'zhoushan',
+        label: '舟山',
+      }],
+    },{
+      value: 'yiwu',
+      label: '义务',
+      children: [{
+        value: 'jinhua',
+        label: '金华',
+      }],
+    },{
+      value: 'changxing',
+      label: '长兴',
+      children: []
+    },{
+      value: 'jiaxing',
+      label: '嘉兴',
+      children: []
+    },{
+      value: 'wenzhou',
+      label: '温州',
+      children: []
+    },{
+      value: 'lishui',
+      label: '丽水',
+      children: []
+    },{
+      value: 'linan',
+      label: '临安',
+      children: []
+    }],
+  }, {
+    value: 'jiangsu',
+    label: '江苏',
+    children: [{
+      value: 'nanjing',
+      label: '南京',
+      children: [{
+        value: 'zhonghuamen',
+        label: '中华门',
+      }],
     }],
   },{
-    value: 'nanjin',
-    label: '南京',
+    value: 'anhui',
+    label: '安徽',
     children: [{
-      value: 'zhonghua',
-      label: '中华',
+      value: 'hefei',
+      label: '合肥',
+      children: [{
+        value: 'dashushan',
+        label: '大蜀山',
+      }],
     }],
-  },{
-    value: 'hefe',
-    label: '合肥',
+  }, {
+    value: 'shandong',
+    label: '山东',
     children: [{
-      value: 'dashusha',
-      label: '大蜀山',
+      value: 'jinan',
+      label: '济南',
+      children: [{
+        value: 'baotuquan',
+        label: '趵突泉',
+      }],
     }],
-  }],
-}, {
-  value: 'jiangsu',
-  label: '江苏',
-  children: [{
-    value: 'nanjing',
-    label: '南京',
-    children: [{
-      value: 'zhonghuamen',
-      label: '中华门',
-    }],
-  }],
-},{
-  value: 'anhui',
-  label: '安徽',
-  children: [{
-    value: 'hefei',
-    label: '合肥',
-    children: [{
-      value: 'dashushan',
-      label: '大蜀山',
-    }],
-  }],
-}, {
-  value: 'shandong',
-  label: '山东',
-  children: [{
-    value: 'jinan',
-    label: '济南',
-    children: [{
-      value: 'baotuquan',
-      label: '趵突泉',
-    }],
-  }],
-}];
+  }
+];
 
 class Demo extends React.Component {
 
@@ -79,7 +104,7 @@ class Demo extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={{ position: 'relative', margin: '15px'}}>
         <CascadeMultiSelect
           options={options}
         />
---
+++ src/CascadeMultiSelect.jsx
@@ -0,0 +1,261 @@
+/**
+ * CascadeMultiSelect Component for uxcore
+ * @author guyunxiang
+ *
+ * Copyright 2015-2016, Uxcore Team, Alinw.
+ * All rights reserved.
+ */
+import React from 'react';
+import classnames from 'classnames';
+class CascadeMultiSelect extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataList: props.options,
+      selectArray: [],
+    };
+  }
+
+  // li点击事件
+  onItemClick(data, level) {
+    const { selectArray } = this.state;
+    if (data.value !== selectArray[level]) {
+      selectArray.splice(level + 1);
+    }
+    selectArray[level] = data.value;
+    this.setState({ selectArray });
+  }
+
+  // checkbox 选中事件
+  onItemChecked(item, level) {
+    item.checked = !item.checked;
+    item.halfChecked = false;
+    if (item.children) {
+      this.setChildrenChecked(item.children, item.checked);
+    }
+    if (level) {
+      this.eachBotherCheckState(item, level, item.checked);
+    }
+    this.setState(this.state);
+  }
+
+  // 清空结果
+  onCleanSelect() {
+    const { dataList } = this.state;
+    if (dataList) {
+      this.cleanResult(dataList);
+    }
+    this.setState(this.state);
+  }
+
+  // 设置children checked
+  setChildrenChecked(data, checked) {
+    if (data && data.length) {
+      data.forEach(item => {
+        item.checked = checked;
+        if (item.children) {
+          this.setChildrenChecked(item.children, checked);
+        }
+      });
+    }
+  }
+
+  // 获取指定key的children
+  getChildrenNode(data, key) {
+    let back = [];
+    if (!key) { return []; }
+    if (data && data.length) {
+      for (let i = 0, len = data.length; i < len; i += 1) {
+        const item = data[i];
+        if (item.value === key) {
+          return item.children;
+        }
+        if (item.children) {
+          const res = this.getChildrenNode(item.children, key);
+          back = res.length ? res : back;
+        }
+      }
+    }
+    return back;
+  }
+
+  // 获取指定节点对象
+  getNodeData(data, key) {
+    let back = null;
+    if (!key) { return null; }
+    if (data && data.length) {
+      for (let i = 0, len = data.length; i < len; i += 1) {
+        if (data[i].value === key) {
+          return data[i];
+        }
+        if (data[i].children) {
+          const item = this.getNodeData(data[i].children, key);
+          back = item ? item : back;
+        }
+      }
+    }
+    return back;
+  }
+
+  // 设置父节点选中状态
+  setFatherCheckState(halfChecked, level, checked) {
+    const { dataList, selectArray } = this.state;
+    const dataObj = this.getNodeData(dataList, selectArray[level - 1]);
+    dataObj.checked = halfChecked ? false : checked;
+    dataObj.halfChecked = halfChecked;
+    if (level - 1) {
+      this.eachBotherCheckState(dataObj, level - 1, checked);
+    }
+  }
+
+  // 遍历兄弟节点
+  eachBotherCheckState(data, level, checked) {
+    const { dataList, selectArray } = this.state;
+    const listArray = this.getChildrenNode(dataList, selectArray[level - 1]);
+    let halfChecked = false;
+    if (listArray) {
+      for (let i = 0, len = listArray.length; i < len; i += 1) {
+        if (listArray[i].checked !== checked) {
+          halfChecked = true;
+        }
+      }
+      this.setFatherCheckState(halfChecked, level, checked);
+    }
+  }
+
+  // 递归清空结果
+  cleanResult(dataList) {
+    if (dataList) {
+      dataList.forEach((item) => {
+        item.checked = false;
+        item.halfChecked = false;
+        if (item.children) {
+          this.cleanResult(item.children);
+        }
+      });
+    }
+  }
+
+  renderUlList(level) {
+    const t = this;
+    const { className, prefixCls } = this.props;
+    const { dataList, selectArray } = this.state;
+    if (!dataList.length) { return null; }
+    const listArray = level ? t.getChildrenNode(dataList, selectArray[level - 1]) : dataList;
+    return (
+      <ul
+        key={level}
+        className={classnames({
+          className: !!className,
+          [`${prefixCls}-content`]: true,
+        }, 'use-svg')}
+      >
+        {t.renderListItems(listArray, level)}
+      </ul>
+    );
+  }
+
+  renderListItems(dataList, level) {
+    const t = this;
+    const { className, prefixCls } = this.props;
+    return dataList.map((item) => {
+      return (
+        <li
+          key={item.value}
+          className={classnames({
+            className: !!className,
+            [`${prefixCls}-list-item`]: !!prefixCls,
+            [`${prefixCls}-checked`]: item.checked && !item.disabled
+          })}
+          title={item.label}
+          onClick={() => {
+            t.onItemClick(item, level);
+          }}
+        >
+          <label
+            className={classnames({
+              className: !!className,
+            }, [`${prefixCls}-item-label`])}
+          >
+            <s
+              className={classnames({
+                className: !!className,
+                'kuma-tree-checkbox': true,
+                'kuma-tree-checkbox-indeterminate': item.halfChecked,
+                'kuma-tree-checkbox-checked': item.checked && !item.halfChecked
+              })}
+              onClick={() => {
+                t.onItemChecked(item, level);
+              }}
+            />
+            {item.label}
+          </label>
+        </li>
+      );
+    });
+  }
+
+  // 渲染结果列表
+  renderResult() {
+    const { prefixCls } = this.props;
+    return (
+      <div
+        className={classnames([`${prefixCls}-result`])}
+      >
+        <div className={classnames([`${prefixCls}-result-title`])}>
+          已选择
+          <span
+            className={classnames([`${prefixCls}-result-clean`])}
+            onClick={() => {
+              this.onCleanSelect();
+            }}
+          >清空</span>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { className, prefixCls, cascadeSize, config } = this.props;
+    const depthSize = config.length || cascadeSize;
+    let arr = [];
+    for (let i = 0; i < depthSize; i += 1) {
+      arr.push(
+        this.renderUlList(i)
+      );
+    }
+    return (
+      <div
+        className={classnames({
+          className: !!className
+        }, [`${prefixCls}`])}
+      >
+        {arr}
+        {this.renderResult()}
+      </div>
+    );
+  }
+}
+
+CascadeMultiSelect.defaultProps = {
+  className: '',
+  prefixCls: 'uxcore-cascade-multi-select',
+  config: [],
+  options: [],
+  cascadeSize: 3,
+};
+
+
+// http://facebook.github.io/react/docs/reusable-components.html
+CascadeMultiSelect.propTypes = {
+  className: React.PropTypes.string,
+  prefixCls: React.PropTypes.string,
+  config: React.PropTypes.array,
+  options: React.PropTypes.array,
+  cascadeSize: React.PropTypes.number,
+};
+
+CascadeMultiSelect.displayName = 'CascadeMultiSelect';
+
+module.exports = CascadeMultiSelect;
