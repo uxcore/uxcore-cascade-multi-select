bef7dcc22c30c00979f4a93e1ab13955c35b86ce
常鸣 <px.zhangyang@gmail.com>
2017-03-27 17:53:14 +0800

    升级新样式

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * CascadeMultiSelect Component for uxcore
  * @author guyunxiang
  *
- * Copyright 2015-2016, Uxcore Team, Alinw.
+ * Copyright 2015-2017, Uxcore Team, Alinw.
  * All rights reserved.
  */
 import CascadeMultiSelect from './CascadeMultiSelect';
